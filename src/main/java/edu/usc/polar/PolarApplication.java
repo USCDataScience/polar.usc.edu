a59ac740b30a28fee9af4d30774ceab4a30ac32e
llsposto <llsposto@usc.edu>
2016-03-08 13:48:38 -0800

    Remove sysout

--- src/main/java/edu/usc/polar/PolarApplication.java
+++ src/main/java/edu/usc/polar/PolarApplication.java
@@ -23,12 +23,12 @@ public class PolarApplication extends WebApplication {
 
         IPackageResourceGuard guard = getResourceSettings().getPackageResourceGuard();
         if (guard instanceof SecurePackageResourceGuard) {
-            System.out.println("HERE GUARD");
+//            System.out.println("HERE GUARD");
             SecurePackageResourceGuard sguard = (SecurePackageResourceGuard) guard;
             sguard.addPattern("+*.tsv");
             sguard.addPattern("+*.json");
             sguard.addPattern("+*.csv");
-            System.out.println(sguard.getPattern());
+//            System.out.println(sguard.getPattern());
         }
 //        final String resourceKey = "data";
 //        getSharedResources().add(resourceKey, new ByteArrayResource("text/plain", "TESTESTtest".getBytes()));

13d1c96aec0dc16b9eef1d7c91a6dd281e67050d
llsposto <llsposto@usc.edu>
2016-03-20 14:11:46 -0700

    Cleanup

--- pytikasolr/tika_parse_solr.py
+++ pytikasolr/tika_parse_solr.py
@@ -6,85 +6,42 @@ import copy
 
 __author__ = 'Lorraine Sposto'
 
-# This script utilizes GeoTopicParser: https://wiki.apache.org/tika/GeoTopicParser
-# Make sure to follow the set up steps at the link above to set up location-ner-model and geotopic-mime.
-#
-# This script queries documents from the Solr source core and extracts geo data from the document's 'content' field.
-# GeoTopicParser returns a set of location names and coordinates. This information is added to each document in the
-# fields 'location_names' and 'location_coordinates' respectively, and the new document is indexed to the destination
-# core.
-#
-# Make sure you have:
-#   - a lucene-geo-gazetteer server running.
-#   - a Tika server running with location-ner-model and geotopic-mime in the classpath. Set the Tika server url below.
-#   - a Solr server running. Set the source core url (the core of documents you want to process)
-#       and the destination core url (where the enriched documents will go).
-#
-# This script assumes Tika is set up with GeoTopicParser. If any of the above is not satisfied, documents will still be
-# indexed into the destination core, but will not have any new data. If you do not configure your own Tika server,
-# tika-python will attempt to start its own server, which will not have the necessary items in the classpath.
-# Additionally, the gazetteer must be running for the Geo extractions to be performed.
-
 # Using:
 # Tika Server 1.12
 # Command: lucene-geo-gazetteer -server
-# Command: java -classpath location-ner-model:geotopic-mime:tika-server-1.12.jar org.apac.tika.server.TikaServerCli
+# Command: java -classpath location-ner-model:geotopic-mime:tika-server-1.12.jar org.apache.tika.server.TikaServerCli
 
-tika_server = 'http://localhost:9998/tika'
-solr_source = pysolr.Solr('http://localhost:8983/solr/collection1', timeout=10)
-solr_dest = pysolr.Solr('http://localhost:8983/solr/test', timeout=10)
-
-
-def main():
-    queries_made = 0
-    start = 0
-    rows = 25
-    hits = 0
-    num_processed = 0
-    num_total_successful = 0
-
-    print 'Start:', start
-    print 'Rows:', rows
-    print '---------------\n'
-
-    while num_processed < hits or num_processed == 0 and hits == 0:
-        # num_docs = extract_geo_rows_and_index_docs(start, rows)
-        print 'Fetching', rows, 'rows from', start
-        r = solr_source.search('*', **{
-            'start': start,
-            'rows': rows
-        })
-        if hits == 0:
-            hits = r.hits
-
-        num_docs = len(r.docs)
-        num_successful = 0
-        for doc in r.docs:
-            geo_enriched_doc = extract_geo(doc)
-
-            if geo_enriched_doc is not None:
-                solr_dest.add([geo_enriched_doc])
-                num_successful += 1
-
-        print 'Indexed', num_successful, 'docs'
-
-        queries_made += 1
-        start += rows
-        num_processed += num_docs
-        num_total_successful += num_successful
-
-    print '\n---------------'
-    print 'Queried Solr', queries_made, 'times'
-    print 'Processed', num_processed, 'docs'
-    print 'Succssfully indexed', num_total_successful, 'docs'
+SOLR_SOURCE = pysolr.Solr('http://polar.usc.edu/solr/team6', timeout=10)
+SOLR_DEST = pysolr.Solr('http://localhost:8983/solr/geo', timeout=10)
 
 
 def extract_geo(doc):
     """
     Uses Tika to extract geo data and returns an updated copy of the document.
+
+    This script utilizes GeoTopicParser: https://wiki.apache.org/tika/GeoTopicParser
+    Make sure to follow the set up steps at the link above to set up location-ner-model and geotopic-mime.
+
+    This script queries documents from the Solr source core and extracts geo data from the document's 'content' field.
+    GeoTopicParser returns a set of location names and coordinates. This information is added to each document in the
+    fields 'location_names' and 'location_coordinates' respectively, and the new document is indexed to the destination
+    core.
+
+    Make sure you have:
+      - a lucene-geo-gazetteer server running.
+      - a Tika server running with location-ner-model and geotopic-mime in the classpath. Set the Tika server url below.
+      - a Solr server running. Set the source core url (the core of documents you want to process)
+          and the destination core url (where the enriched documents will go).
+
+    This script assumes Tika is set up with GeoTopicParser. If any of the above is not satisfied, documents will still be
+    indexed into the destination core, but will not have any new data. If you do not configure your own Tika server,
+    tika-python will attempt to start its own server, which will not have the necessary items in the classpath.
+    Additionally, the gazetteer must be running for the Geo extractions to be performed.
+
     :param doc: A document from solr, as a dict
     :return: Updated document, as a dict, or None if error
     """
+    tika_server = 'http://localhost:9998/tika'
     try:
         names = []
         coords = []
@@ -109,7 +66,6 @@ def extract_geo(doc):
                                     geo_groups[n][attr] = item[key]
 
                             for key, value in geo_groups.iteritems():
-                                # print value
                                 geokeys = value.keys()
                                 if 'name' in geokeys:
                                     names.append(value['name'])
@@ -126,11 +82,62 @@ def extract_geo(doc):
         del enriched['_version_']
         enriched['location_name'] = names
         enriched['location_coordinates'] = coords
+
         return enriched
     except Exception as e:
         print e.message
         return None
 
+
+def process_solr_docs():
+    queries_made = 0
+    start = 0
+    rows = 25
+    hits = 0
+    num_processed = 0
+    num_total_successful = 0
+
+    print 'Start:', start
+    print 'Rows:', rows
+    print '---------------\n'
+
+    while num_processed < hits or num_processed == 0 and hits == 0:
+        print 'Fetching', rows, 'rows from', start
+        r = SOLR_SOURCE.search('*', **{
+            'start': start,
+            'rows': rows
+        })
+        if hits == 0:
+            hits = r.hits
+
+        num_docs = len(r.docs)
+        print 'Processing', num_docs, 'docs'
+        num_successful = 0
+        for doc in r.docs:
+            geo_enriched_doc = extract_geo(doc)
+
+            if geo_enriched_doc is not None:
+                boost = None
+                if 'boost' in geo_enriched_doc.keys():
+                    boost = geo_enriched_doc['boost']
+                    del geo_enriched_doc['boost']
+                SOLR_DEST.add([geo_enriched_doc], boost=boost)
+                num_successful += 1
+
+        queries_made += 1
+        start += rows
+        num_processed += num_docs
+        num_total_successful += num_successful
+        print 'Indexed', num_processed, '/', hits, 'docs'
+
+    print '\n---------------'
+    print 'Hit Solr', queries_made, 'times'
+    print 'Processed', num_processed, 'docs'
+    print 'Successfully indexed', num_total_successful, 'docs'
+
+
 if __name__ == "__main__":
-    main()
+    process_solr_docs()
+    # parse_date_time()
+
     print 'Exiting...'

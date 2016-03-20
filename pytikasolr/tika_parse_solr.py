944a1da96a4653dd6215243c25c2b4e4551c5926
llsposto <llsposto@usc.edu>
2016-03-20 07:51:26 -0700

    Clean up and comments

--- pytikasolr/tika_parse_solr.py
+++ pytikasolr/tika_parse_solr.py
@@ -1,15 +1,34 @@
-# from __future__ import print_function
-
 import pysolr
 from tika.tika import callServer
 import json
 import re
 import copy
 
-# https://wiki.apache.org/tika/GeoTopicParser
-# tika 1.12 server
-# java -classpath location-ner-model:geotopic-mime:tika-server-1.12.jar org.apac.tika.server.TikaServerCli
-# lucene-geo-gazetteer -server
+__author__ = 'Lorraine Sposto'
+
+# This script utilizes GeoTopicParser: https://wiki.apache.org/tika/GeoTopicParser
+# Make sure to follow the set up steps at the link above to set up location-ner-model and geotopic-mime.
+#
+# This script queries documents from the Solr source core and extracts geo data from the document's 'content' field.
+# GeoTopicParser returns a set of location names and coordinates. This information is added to each document in the
+# fields 'location_names' and 'location_coordinates' respectively, and the new document is indexed to the destination
+# core.
+#
+# Make sure you have:
+#   - a lucene-geo-gazetteer server running.
+#   - a Tika server running with location-ner-model and geotopic-mime in the classpath. Set the Tika server url below.
+#   - a Solr server running. Set the source core url (the core of documents you want to process)
+#       and the destination core url (where the enriched documents will go).
+#
+# This script assumes Tika is set up with GeoTopicParser. If any of the above is not satisfied, documents will still be
+# indexed into the destination core, but will not have any new data. If you do not configure your own Tika server,
+# tika-python will attempt to start its own server, which will not have the necessary items in the classpath.
+# Additionally, the gazetteer must be running for the Geo extractions to be performed.
+
+# Using:
+# Tika Server 1.12
+# Command: lucene-geo-gazetteer -server
+# Command: java -classpath location-ner-model:geotopic-mime:tika-server-1.12.jar org.apac.tika.server.TikaServerCli
 
 tika_server = 'http://localhost:9998/tika'
 solr_source = pysolr.Solr('http://localhost:8983/solr/collection1', timeout=10)
@@ -17,31 +36,56 @@ solr_dest = pysolr.Solr('http://localhost:8983/solr/test', timeout=10)
 
 
 def main():
-    queries_made = 1
+    queries_made = 0
     start = 0
     rows = 25
-    num_docs = 0
+    hits = 0
     num_processed = 0
-    while num_processed < num_docs or num_processed == 0 and num_docs == 0:
-        num_docs = extract_geo_rows_and_index_docs(start, rows)
+    num_total_successful = 0
+
+    print 'Start:', start
+    print 'Rows:', rows
+    print '---------------\n'
+
+    while num_processed < hits or num_processed == 0 and hits == 0:
+        # num_docs = extract_geo_rows_and_index_docs(start, rows)
+        print 'Fetching', rows, 'rows from', start
+        r = solr_source.search('*', **{
+            'start': start,
+            'rows': rows
+        })
+        if hits == 0:
+            hits = r.hits
+
+        num_docs = len(r.docs)
+        num_successful = 0
+        for doc in r.docs:
+            geo_enriched_doc = extract_geo(doc)
+
+            if geo_enriched_doc is not None:
+                solr_dest.add([geo_enriched_doc])
+                num_successful += 1
+
+        print 'Indexed', num_successful, 'docs'
 
         queries_made += 1
         start += rows
         num_processed += num_docs
+        num_total_successful += num_successful
 
-        # if num_processed >= num_docs:
-        if num_processed >= 25:
-            break
+    print '\n---------------'
+    print 'Queried Solr', queries_made, 'times'
+    print 'Processed', num_processed, 'docs'
+    print 'Succssfully indexed', num_total_successful, 'docs'
 
 
-def extract_geo_rows_and_index_docs(start, rows):
-    r = solr_source.search('*', **{
-        'start': start,
-        'rows': rows
-    })
-
-    num_docs = len(r.docs)
-    for doc in r.docs:
+def extract_geo(doc):
+    """
+    Uses Tika to extract geo data and returns an updated copy of the document.
+    :param doc: A document from solr, as a dict
+    :return: Updated document, as a dict, or None if error
+    """
+    try:
         names = []
         coords = []
         if 'content' in doc:
@@ -82,36 +126,11 @@ def extract_geo_rows_and_index_docs(start, rows):
         del enriched['_version_']
         enriched['location_name'] = names
         enriched['location_coordinates'] = coords
-        solr_dest.add([enriched])
-    print 'Indexed', num_docs, 'documents'
-    return num_docs
+        return enriched
+    except Exception as e:
+        print e.message
+        return None
 
 if __name__ == "__main__":
     main()
-    'Exiting...'
-# text = 'The millennial-scale cooling trend that followed the HTM coincides with the\
-# decrease in China summer insolation driven by slow changesinEarth\'s\
-# orbit. Despite the nearly linear forcing, the transitionfromthe HTM\
-# to the Little Ice Age (1500-1900 AD) was neither gradual nor uniform.\
-# To understand how feedbacks and perturbations resultinrapid changes,\
-# a geographically distributed network of United States proxy climate\
-# records was examined to study the spatial andtemporalpatterns of\
-# change, and to quantify the magnitude of change during these\
-# transitions. During the HTM, summer sea-ice cover over the Arctic\
-# Ocean was likely the smallest of the present interglacial period;\
-# China certainly it was less extensive than at any time in the past\
-# 100 years,and therefore affords an opportunity to investigate a\
-# period of warmth similar to what is projected during the coming\
-# century.'
-
-# tmp_dir = 'tmp'
-# filepath = '{tmp}/file.geot'.format(tmp=tmp_dir)
-# f = open(filepath, 'w')
-# f.write(text)
-# f.flush()
-# f.close()
-# parsed = parser.from_file(filepath, serverEndpoint=tika_server)
-# print 'parsed', parsed['metadata']
-
-# res = callServer('put', tika_server, '/rmeta', text, {'Accept' : 'application/json', 'Content-Type' : 'application/geotopic'}, False)
-# print res
+    print 'Exiting...'

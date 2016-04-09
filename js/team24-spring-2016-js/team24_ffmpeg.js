23f9f94c7e7249d4a19e3021dd114b495275e303
skashyap7 <sumankashyap22@gmail.com>
2016-04-08 20:56:20 -0700

    Adding TREC Enrichment visualization for Team 24

---
+++ js/team24-spring-2016-js/team24_ffmpeg.js
@@ -0,0 +1,16 @@
+var layout_data = [
+    { "len": 231, "color": "#8dd3c7", "label": "xmpdm:duration", "id": "duration" },
+    { "len": 231, "color": "#bebada", "label": "xmpdm:videocompressor", "id": "videocompressor" },
+    { "len": 231, "color": "#80b1d3", "label": "xmpdm:audiosamplerate", "id": "audiosamplerate" },
+    { "len": 231, "color": "#b3de69", "label": "xmpdm:audiocompressor", "id": "audiocompressor" },
+    { "len": 231, "color": "#fccde5", "label": "xmpdm:videoframerate", "id": "videoframerate" },
+    { "len": 231, "color": "#fccde5", "label": "xmpdm:filedatarate", "id": "filedatarate" }
+];
+var heatmap = [
+    ['duration',0,214,214],
+    ['videocompressor',0,231,231],
+    ['audiosamplerate',0,47,47],
+    ['audiocompressor',0,47,47],
+    ['videoframerate',0,138,138],
+    ['filedatarate',0,231,231]
+];

8a2f4b0ba79f4a31ed2a0287b55a212ce6b8baf8
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-03 06:11:33 -0700

    Modifed the location of the word cloud.

--- EvalTrec_Team25/js/metadata_word_cloud.js
+++ EvalTrec_Team25/js/metadata_word_cloud.js
@@ -41,7 +41,7 @@ $(function(){
             .domain([0,1,2,3,4,5,6,10,15,20,100])
             .range(["red", "gold", "orange", "blue", "cyan", "lightblue", "black", "green", "pink", "purple", "yellow", "grey"]);
 
-        d3.layout.cloud().size([1000, 500])
+        d3.layout.cloud().size([700, 400])
             .words(frequency_list)
             .rotate(0)
             .fontSize(function(d) { return d.size; })
@@ -50,13 +50,13 @@ $(function(){
 
         function draw(words) {
             d3.select("body").append("svg")
-                .attr("width", 1050)
+                .attr("width", 1250)
                 .attr("height", 550)
                 .attr("class", "wordcloud")
                 .append("g")
                 // without the transform, words words would get cutoff to the left and top, they would
                 // appear outside of the SVG area
-                .attr("transform", "translate(520,200)")
+                .attr("transform", "translate(470,250)")
                 .selectAll("text")
                 .data(words)
                 .enter().append("text")

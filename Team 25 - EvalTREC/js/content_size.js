c9d63e329cef4549a0eba97f97bf70f5c961731a
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-05-10 11:11:05 -0700

    Adding the visualizations.

---
+++ Team 25 - EvalTREC/js/content_size.js
@@ -0,0 +1,37 @@
+/**
+ * Created by manishdwibedy on 4/5/16.
+ */
+
+
+$(function(){
+
+    $(".dropdown-menu li a").click(function(){
+
+        $(".btn:first-child").text('Selected MIME : '+$(this).text());
+        $(".btn:first-child").val($(this).text());
+
+        var mime = $(this).text();
+
+        loadData(mime);
+    });
+
+});
+
+function loadData(mime) {
+
+    var chart = c3.generate({
+        bindto: '#chart',
+        data: {
+            url: 'data/content_size/' + mime + '.json',
+            mimeType: 'json',
+            type : 'donut',
+            onclick: function (d, i) { console.log("onclick", d, i); },
+            onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+            onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+        },
+        donut: {
+            title: mime.toUpperCase()
+        }
+    });
+
+}
